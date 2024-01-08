5be73f1fb383c3c5440190332ac2d25e362daace
Leon Si <leon@leondreamed.com>
2024-01-08 15:47:45 -0500

    fix: patch

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,7 +4,10 @@ import fs from "node:fs";
 import downl from "downl";
 import applyPatchesExports from "patch-package/dist/applyPatches.js";
 import { join } from "desm";
+import monorepoPackageJson from "../package.json";
+import { generatePatch } from "./utils/patch.js";
 
+await generatePatch();
 const { applyPatch } = applyPatchesExports;
 
 fs.rmSync("build", { force: true, recursive: true });
@@ -14,7 +17,18 @@ await downl(`https://registry.npmjs.org/tsx/-/tsx-${version}.tgz`, "build", {
   extract: { strip: 1 },
 });
 
-// applyPatch({
-//   patchFilePath: join(import.meta.url, "../generated/tsx@4.7.0.patch"),
-//   cwd: "build",
-// });
+applyPatch({
+  patchFilePath: join(import.meta.url, "../generated/tsx@4.7.0.patch"),
+  cwd: "build",
+});
+
+const packageJson = JSON.parse(
+  await fs.promises.readFile("build/package.json", "utf8")
+);
+packageJson.name = "@tunnel/tun";
+packageJson.version = monorepoPackageJson.version;
+
+await fs.promises.writeFile(
+  "build/package.json",
+  JSON.stringify(packageJson, null, 2)
+);
--- scripts/utils/patch.ts
+++ scripts/utils/patch.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env -S pnpm exec tsx
-
 import fs from "node:fs";
 import downl from "downl";
 import { execa } from "execa";
@@ -9,43 +7,44 @@ import tmp from "tmp-promise";
 import * as replaceInFile from "replace-in-file";
 import escapeStringRegexp from "escape-string-regexp";
 
-const temporaryDirectory = await tmp.dir();
-const temporarySourceDirpath = path.join(temporaryDirectory.path, "a");
-const temporaryPatchDirpath = path.join(temporaryDirectory.path, "b");
+export async function generatePatch() {
+  const temporaryDirectory = await tmp.dir();
+  const temporarySourceDirpath = path.join(temporaryDirectory.path, "a");
+  const temporaryPatchDirpath = path.join(temporaryDirectory.path, "b");
 
-const version = "4.7.0";
-await downl(
-  `https://registry.npmjs.org/tsx/-/tsx-${version}.tgz`,
-  temporarySourceDirpath,
-  { extract: { strip: 1 } }
-);
+  const version = "4.7.0";
+  await downl(
+    `https://registry.npmjs.org/tsx/-/tsx-${version}.tgz`,
+    temporarySourceDirpath,
+    { extract: { strip: 1 } }
+  );
 
-await fs.cpSync(temporarySourceDirpath, temporaryPatchDirpath, {
-  recursive: true,
-});
-
-const replace = function (
-  options: Parameters<(typeof replaceInFile)["replaceInFile"]>[0]
-) {
-  return (replaceInFile.default ?? replaceInFile).replaceInFileSync({
-    ...options,
-    files: [options.files]
-      .flat()
-      .map((file) => path.join(temporaryPatchDirpath, file)),
-    from: [options.from]
-      .flat()
-      .map((file) =>
-        typeof file === "string"
-          ? new RegExp(escapeStringRegexp(file).replaceAll(/\s+/g, "\\s+"))
-          : file
-      ),
+  await fs.cpSync(temporarySourceDirpath, temporaryPatchDirpath, {
+    recursive: true,
   });
-};
 
-replace({
-  files: "package.json",
-  from: ['"dependencies": {'],
-  to: outdent`
+  const replace = function (
+    options: Parameters<(typeof replaceInFile)["replaceInFile"]>[0]
+  ) {
+    return (replaceInFile.default ?? replaceInFile).replaceInFileSync({
+      ...options,
+      files: [options.files]
+        .flat()
+        .map((file) => path.join(temporaryPatchDirpath, file)),
+      from: [options.from]
+        .flat()
+        .map((file) =>
+          typeof file === "string"
+            ? new RegExp(escapeStringRegexp(file).replaceAll(/\s+/g, "\\s+"))
+            : file
+        ),
+    });
+  };
+
+  replace({
+    files: "package.json",
+    from: ['"dependencies": {'],
+    to: outdent`
 		"dependencies": {
 			"is-file-esm-ts": "^0.1.0",
 			"tilde-imports": "^3.1.3",
@@ -54,12 +53,12 @@ replace({
 			"resolve.exports": "^2.0.2",
 			"monorepo-packages": "^1.1.0",
 	`,
-});
+  });
 
-replace({
-  files: "dist/esm/index.mjs",
-  from: /^/,
-  to: outdent({ trimTrailingNewline: false })`
+  replace({
+    files: "dist/esm/index.mjs",
+    from: /^/,
+    to: outdent({ trimTrailingNewline: false })`
 		import { createTildeImportExpander } from 'tilde-imports';
 		import { isGlobSpecifier, createGlobfileManager } from 'glob-imports';
 		import { getMonorepoDirpath } from 'get-monorepo-root';
@@ -81,12 +80,12 @@ replace({
 			monorepoDirpath
 		});
 	`,
-});
+  });
 
-replace({
-  files: "dist/esm/index.cjs",
-  from: /^/,
-  to: outdent({ trimTrailingNewline: false })`
+  replace({
+    files: "dist/esm/index.cjs",
+    from: /^/,
+    to: outdent({ trimTrailingNewline: false })`
 		const { createTildeImportExpander } = require('tilde-imports');
 		const { isGlobSpecifier, createGlobfileManager } = require('glob-imports');
 		const { getMonorepoDirpath } = require('get-monorepo-root');
@@ -108,12 +107,12 @@ replace({
 			monorepoDirpath
 		});
 	`,
-});
+  });
 
-replace({
-  files: ["dist/esm/index.mjs", "dist/esm/index.cjs"],
-  from: /(?:O|F)=async function\((\w,\w,\w,\w)\)\{/,
-  to: outdent({ trimTrailingNewline: false })`
+  replace({
+    files: ["dist/esm/index.mjs", "dist/esm/index.cjs"],
+    from: /(?:O|F)=async function\((\w,\w,\w,\w)\)\{/,
+    to: outdent({ trimTrailingNewline: false })`
 		O=async function($1){
 			const [specifier, context, defaultResolve, recursiveCall] = arguments;
 			if (specifier.includes('/node_modules/')) {
@@ -182,12 +181,12 @@ replace({
 				}
 			}
 	`,
-});
+  });
 
-replace({
-  files: ["dist/esm/index.mjs"],
-  from: "C();",
-  to: outdent({ trimTrailingNewline: false })`
+  replace({
+    files: ["dist/esm/index.mjs"],
+    from: "C();",
+    to: outdent({ trimTrailingNewline: false })`
 		import path from 'node:path';
 		import { createRequire } from 'node:module';
 		import { isFileEsmSync } from 'is-file-esm-ts';
@@ -209,85 +208,86 @@ replace({
 			registerLoader();
 		}
 	`,
-});
+  });
 
-replace({
-  files: ["dist/cjs/index.cjs", "dist/cjs/index.mjs"],
-  from: "enumerable:!1",
-  to: outdent`
+  replace({
+    files: ["dist/cjs/index.cjs", "dist/cjs/index.mjs"],
+    from: "enumerable:!1",
+    to: outdent`
 		$&,
 		// We set this property as enumerable so other packages can overwrite it if needed instead of erroring
 		writable: true,
 		configurable: true,
 	`,
-});
+  });
 
-const { stdout } = await execa(
-  "/usr/bin/git",
-  [
-    "-c",
-    "core.safecrlf=false",
-    "diff",
-    "--src-prefix=a/",
-    "--dst-prefix=b/",
-    "--ignore-cr-at-eol",
-    "--irreversible-delete",
-    "--full-index",
-    "--no-index",
-    "--text",
-    temporarySourceDirpath,
-    temporaryPatchDirpath,
-  ],
-  {
-    reject: false,
-    env: {
-      ...process.env,
-      // These variables aim to ignore the global git config so we get predictable output
-      // https://git-scm.com/docs/git#Documentation/git.txt-codeGITCONFIGNOSYSTEMcode
-      GIT_CONFIG_NOSYSTEM: "1",
-      HOME: "",
-      XDG_CONFIG_HOME: "",
-      USERPROFILE: "",
-    },
-    stripFinalNewline: false,
-  }
-);
+  const { stdout } = await execa(
+    "/usr/bin/git",
+    [
+      "-c",
+      "core.safecrlf=false",
+      "diff",
+      "--src-prefix=a/",
+      "--dst-prefix=b/",
+      "--ignore-cr-at-eol",
+      "--irreversible-delete",
+      "--full-index",
+      "--no-index",
+      "--text",
+      temporarySourceDirpath,
+      temporaryPatchDirpath,
+    ],
+    {
+      reject: false,
+      env: {
+        ...process.env,
+        // These variables aim to ignore the global git config so we get predictable output
+        // https://git-scm.com/docs/git#Documentation/git.txt-codeGITCONFIGNOSYSTEMcode
+        GIT_CONFIG_NOSYSTEM: "1",
+        HOME: "",
+        XDG_CONFIG_HOME: "",
+        USERPROFILE: "",
+      },
+      stripFinalNewline: false,
+    }
+  );
 
-function removeTrailingAndLeadingSlash(p: string) {
-  if (p.startsWith("/") || p.endsWith("/")) {
-    return p.replace(/^\/|\/$/g, "");
+  function removeTrailingAndLeadingSlash(p: string) {
+    if (p.startsWith("/") || p.endsWith("/")) {
+      return p.replace(/^\/|\/$/g, "");
+    }
+    return p;
   }
-  return p;
+
+  fs.writeFileSync(
+    `generated/tsx@${version}.patch`,
+    stdout
+      .replace(
+        new RegExp(
+          `(a|b)(${escapeStringRegexp(
+            `/${removeTrailingAndLeadingSlash(temporarySourceDirpath)}/`
+          )})`,
+          "g"
+        ),
+        "$1/"
+      )
+      .replace(
+        new RegExp(
+          `(a|b)${escapeStringRegexp(
+            `/${removeTrailingAndLeadingSlash(temporaryPatchDirpath)}/`
+          )}`,
+          "g"
+        ),
+        "$1/"
+      )
+      .replace(
+        new RegExp(escapeStringRegexp(`${temporarySourceDirpath}/`), "g"),
+        ""
+      )
+      .replace(
+        new RegExp(escapeStringRegexp(`${temporaryPatchDirpath}/`), "g"),
+        ""
+      )
+      .replace(/\n\\ No newline at end of file\n$/, "\n")
+  );
 }
-
-fs.writeFileSync(
-  `generated/tsx@${version}.patch`,
-  stdout
-    .replace(
-      new RegExp(
-        `(a|b)(${escapeStringRegexp(
-          `/${removeTrailingAndLeadingSlash(temporarySourceDirpath)}/`
-        )})`,
-        "g"
-      ),
-      "$1/"
-    )
-    .replace(
-      new RegExp(
-        `(a|b)${escapeStringRegexp(
-          `/${removeTrailingAndLeadingSlash(temporaryPatchDirpath)}/`
-        )}`,
-        "g"
-      ),
-      "$1/"
-    )
-    .replace(
-      new RegExp(escapeStringRegexp(`${temporarySourceDirpath}/`), "g"),
-      ""
-    )
-    .replace(
-      new RegExp(escapeStringRegexp(`${temporaryPatchDirpath}/`), "g"),
-      ""
-    )
-    .replace(/\n\\ No newline at end of file\n$/, "\n")
-);
