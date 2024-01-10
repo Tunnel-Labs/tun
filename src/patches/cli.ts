cd7f59f82be1f2a23491541ef57a42e9f7d891c5
Leon Si <leon@leondreamed.com>
2024-01-10 10:56:47 -0500

    fix: patch

--- src/patches/cli.ts
+++ src/patches/cli.ts
@@ -2,7 +2,7 @@ export default [
 	// We can't create IPC servers on read-only file systems
   {
     files: ["dist/cli.cjs", "dist/cli.mjs"],
-    from: ".listen(",
-    to: ".listen.bind(",
+    from: /await yn\(\)/g,
+    to: "({ on() {} })",
   },
 ];
